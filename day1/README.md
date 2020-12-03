dacedb03c813e0a80d016547daf9d2996c5c6451
Dean C <dcmexy@gmail.com>
2020-12-02 20:13:23 -0500

    trying out the recursive approach

---
+++ day1/README.md
@@ -0,0 +1,4 @@
+**Questions**
+
+- Part 1: Find the two entries that sum to 2020; what do you get if you multiply them together?
+- Part 2: What is the product of the three entries that sum to 2020?
--- day1/solution.py
+++ day1/solution.py
@@ -1,6 +1,6 @@
 # Rough solution
 
-# Part 2
+# Part 1 - Using for loops
 
 
 def part1(nums, target):
@@ -10,7 +10,7 @@ def part1(nums, target):
                 return nums[i] * nums[j]
     return 0
 
-# Part 2
+# Part 2 - Using for loops
 
 
 def part2(nums, target):
@@ -22,6 +22,27 @@ def part2(nums, target):
     return 0
 
 
+# Recursive method to allow for flexible numbers to sum.
+def recursiveSolution(nums, target, parts):
+    # parts = how many numbers to sum
+    if len(nums) == 0 or parts == 0 or target == 0:
+        return 0
+
+    # Get first item in the list
+    head = nums[0]
+    tail = nums[1:]
+    # print("Target: ", target)
+
+    # Check if head matches target
+    if head == target and parts == 1:
+        return head
+
+    val = recursiveSolution(tail, target - head, parts - 1)
+    if val > 0:
+        return val * head
+    return recursiveSolution(tail, target, parts)
+
+
 # Read file
 with open('input.txt') as f:
     nums = []
@@ -29,4 +50,7 @@ with open('input.txt') as f:
         nums.append(int(line.strip('\n')))
 
 # Execute
-print(part2(nums, 2020))
+print("Solution 1 with for loop:", part1(nums, 2020))
+print("Solution 2 with for loop:", part2(nums, 2020))
+print("Solution 1 with recursive:", recursiveSolution(nums, 2020, 2))
+print("Solution 2 with recursive:", recursiveSolution(nums, 2020, 3))
