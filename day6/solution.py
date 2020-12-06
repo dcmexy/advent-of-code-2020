c5a4d13207b7761ee3add39456e6d09a38b510c4
Dean C <dcmexy@gmail.com>
2020-12-06 18:49:42 -0500

    Full solution for day 6

--- day6/solution.py
+++ day6/solution.py
@@ -4,16 +4,40 @@
 with open('input.txt') as f:
     lines = f.read().splitlines()
     numberOfLines = len(lines)
-    groups = []
+    anyoneYes = []
+    everyoneYes = []
+    questions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     yeses = ''
+    yesCount = 0
 
     for i, line in enumerate(lines):
         val = line.strip('\n')
-        yeses += val
+
+        # Track yesses
+        if val != '':
+            yesCount += 1
+            yeses += val
 
         # Break into groups when empty line or EOF
         if val == '' or i == numberOfLines - 1:
-            groups.append(len(set(yeses)))
-            yeses = ''
 
-    print("Sum of counts:", sum(groups))
+            # Get count yes by anyone
+            anyoneYes.append(len(set(yeses)))
+
+            # Check for yes by everyone
+            allYes = 0
+            for q in questions:
+                if q in yeses and yeses.count(q) == yesCount:
+                    allYes += 1
+
+            # Get count of yes by everyone
+            everyoneYes.append(allYes)
+
+            # Reset trackers
+            yeses = ''
+            yesCount = 0
+
+    # Print answers
+    print("Sum of Anyone Yes:", sum(anyoneYes))
+    print("Sum of Everyone Yes:", sum(everyoneYes))
