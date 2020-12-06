af6b0f0f67bdb9c2bd6bff62fa5c913f708bc6c6
Dean C <dcmexy@gmail.com>
2020-12-05 21:32:46 -0500

    full solution for day 5

---
+++ day5/README.md
@@ -0,0 +1,5 @@
+# Advent of Code - Day 5
+
+This solution attempts solve the problem posed for Day 5 of Advent of Code 2020
+
+It is a two-part problem set. Full problem statements can be seen here - https://adventofcode.com/2020/day/5
--- day5/solution.py
+++ day5/solution.py
@@ -1,4 +1,4 @@
-#  Part 1 Solution
+#  Complete Solution
 #  Get Seat Id
 
 
@@ -27,16 +27,35 @@ def getSeatId(boardingPass):
         # Track lower half columns
         if i == 'L':
             colCount = colCount // 2
-    # print((rowTrack * 8) + colTrack)
+
+    if rowTrack > 200:
+        print(rowTrack, colTrack, (rowTrack * 8) + colTrack)
     return (rowTrack * 8) + colTrack
 
 
 # Read data from file
 with open('input.txt') as f:
     highestSeatId = 0
+    lowestSeatId = (120 * 8) + 8
+    seatIds = []  # Seat Id pool
     for line in f:
         seatId = getSeatId(line.strip())
+
+        # Add Seat Id to pool
+        seatIds.append(seatId)
+
+        # Track Highest Seat Id
         if seatId > highestSeatId:
             highestSeatId = seatId
 
+        # Track Lowest Seat Id
+        if seatId < lowestSeatId:
+            lowestSeatId = seatId
+
+    # Find Missing Seat
+    for x in range(lowestSeatId, highestSeatId + 1):
+        if x not in seatIds and (x + 1) in seatIds and (x - 1) in seatIds:
+            print("Missing Seat Id", x)
+
     print("Highest Seat Id: ", highestSeatId)
+    print("Lowest Seat Id: ", lowestSeatId)
