96d2456af1ce86f359224c649b110b80ce7d583d
Dean C <dcmexy@gmail.com>
2020-12-07 16:59:55 -0500

    update with day 7 full solution

--- day7/solution.py
+++ day7/solution.py
@@ -1,9 +1,13 @@
 # Part 1 solution for day 7.
+
+# initialize global bag repository
 bags = {}
+
+# process and transform data
 
 
 def processBag(bag):
-    #parentBag = bag[0]
+    # parentBag = bag[0]
     # Get only the parent bag (container) name to eliminate plurality
     # E.g. shinyy gold for both shiny gold bag and shiny gold bags
     parentBag = ' '.join(bag[0].split(' ')[0:-1])
@@ -18,6 +22,8 @@ def processBag(bag):
 
     # return a dictionart of container bags with contents
     return dict([[parentBag, childBags]])
+
+# Part 1 Solution - find bags containing at least one 'shiny gold' bag
 
 
 def findBags(bagsToSearch, bagCount):
@@ -34,10 +40,24 @@ def findBags(bagsToSearch, bagCount):
     # return number of bags tracked less the initial 'shiny gold'
     return len(bagsToSearch) - 1
 
+# Part 2 Solution - find how many individual bags inside a single 'shiny gold' bag
 
-    # Read data from file
+
+def findBags2(bagsToSearch):
+    val = 0
+    for bag, numberOfBags in bags[bagsToSearch].items():
+        if 'no' in numberOfBags:
+            # print(numberOfBags)
+            return 0
+        bagName = ' '.join(bag.split(' ')[0:-1])
+        val += findBags2(bagName) * int(numberOfBags) + int(numberOfBags)
+    return val
+
+
+# Read data from file
 with open('input.txt') as f:
     for line in f:
         bags.update(processBag(line.strip('.\n').split(' contain ')))
 
-    print(findBags(['shiny gold'], 0))
+    print("Part 1 Solution -", findBags(['shiny gold'], 0))
+    print("Part 2 Solution -", findBags2('shiny gold'))
