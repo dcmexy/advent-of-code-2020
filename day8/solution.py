90eac7d9c84686b7deabba893a6364585d213bcb
Dean C <dcmexy@gmail.com>
2020-12-09 13:59:03 -0500

    Day 8 full solution - refactored

--- day8/solution.py
+++ day8/solution.py
@@ -25,86 +25,60 @@ def mutateInstruction(action, pos, acc):
 # function to process Task 1
 
 
-def processInstruction(pos, acc):
+def processInstruction(pos, acc, task=1):
     # Check for empty or negative indices
     if not instructions or pos < 0:
-        return 0
+        return False
 
     # Check that instruction has not executed run at least once
     if pos in visitedPos:
+        return False if task == 2 else acc
+
+    # Check for normal termination
+    if pos >= len(instructions) and task == 2:
         return acc
 
     # Track current instruction
     visitedPos.append(pos)
 
     # unpack instruction and execute accordingly
-    # print(pos)
+
     if instructions[pos][0] == 'nop':
         # process next item
-        return processInstruction(pos + 1, acc)
-
-    if instructions[pos][0] == 'acc':
-        # adjust acc and process next item
-        acc += int(instructions[pos][1])
-        return processInstruction(pos + 1, acc)
-
-    if instructions[pos][0] == 'jmp':
-        # skip to next item
-        return processInstruction(pos + int(instructions[pos][1]), acc)
-
-    return acc
-
-
-def processInstruction2(pos, acc):
-
-    if not instructions or pos < 0:
-        return False
-
-    if pos in visitedPos:
-        return False
-
-    if pos >= len(instructions):
-        # print("here")
-        return acc
-
-    visitedPos.append(pos)
-
-    if instructions[pos][0] == 'nop':
-        nopVal = processInstruction2(pos + 1, acc)
-        if not nopVal and int(instructions[pos][1]) != 0:
+        nopVal = processInstruction(pos + 1, acc, task)
+        if not nopVal and task == 2 and int(instructions[pos][1]) != 0:
             return mutateInstruction('jmp', pos, acc)
         return nopVal
+
     if instructions[pos][0] == 'acc':
-        # adjust acc and process next item
+        # adjust acc, then skip and process next item
         acc += int(instructions[pos][1])
-        return processInstruction2(pos + 1, acc)
+        return processInstruction(pos + 1, acc, task)
 
     if instructions[pos][0] == 'jmp':
         # skip to next item
-        # return processInstruction2(pos + int(instructions[pos][1]), acc)
-
-        jmpVal = processInstruction2(pos + int(instructions[pos][1]), acc)
-        if not jmpVal:
+        jmpVal = processInstruction(pos + int(instructions[pos][1]), acc, task)
+        if not jmpVal and task == 2:
             return mutateInstruction('nop', pos, acc)
         return jmpVal
 
-    # print(instructions[pos])
     return acc
 
 
 def processMutatedInstruction(pos, acc, inst):
-    # print(pos, inst[pos])
-    # return False
+    # Check for empty or negative indices
     if not inst or pos < 0:
         return False
 
+    # Check that mutated instruction has not executed run at least once
     if pos in mutatedVisit:
         return False
 
+    # Check for normal termination
     if pos >= len(inst):
-        # print("here")
         return acc
 
+    # Track current mutated instruction
     mutatedVisit.append(pos)
 
     if inst[pos][0] == 'nop':
@@ -129,6 +103,8 @@ with open('input.txt') as f:
     for line in f:
         instructions.append(line.strip('.\n').split(' '))
 
-
-print(processInstruction2(0, 0))
-print(processInstruction(0, 0))
+# Execute Tasks processInstruction(pos, acc, task)
+# For Task 2
+print(processInstruction(0, 0, 2))
+# For Task 1
+# print(processInstruction(0, 0))
