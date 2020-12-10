a33f94139e31d02582d0cfb542f4d60ef8f3c323
Dean C <dcmexy@gmail.com>
2020-12-10 13:51:39 -0500

    Full solution for day 9

--- day9/solution.py
+++ day9/solution.py
@@ -21,10 +21,32 @@ def processCipher(data, p):
         i += 1
     return False
 
+# Part 2 of Day 10 solution
 
+
+def process2(data, res):
+    for i in range(len(data)):
+        total = data[i]
+        js = [data[i]]
+        for j in range(i + 1, len(data)):
+            total += data[j]
+            js.append(data[j])
+            if total >= res:
+                break
+
+        if total == res:
+            # return the max and min of range
+            return min(js) + max(js)
+
+
+# Read data from file
 with open('input.txt') as f:
     data = []
     for line in f:
         data.append(int(line.strip()))
     # Execute processCipher is a preamble of 25
-    print(processCipher(data, 25))
+    res = processCipher(data, 25)
+    print(res)
+
+    # execute part 2
+    print(process2(data, res))
