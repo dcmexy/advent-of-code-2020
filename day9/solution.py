3fda9f5b4cc63b0753102476da97ce63554881b3
Dean C <dcmexy@gmail.com>
2020-12-09 22:49:27 -0500

    initial solution for Day 9 - part 1

---
+++ day9/solution.py
@@ -0,0 +1,30 @@
+# Solution for Day 9 - Part 1
+
+# function to process the data (cipher)
+# takes in data and preamble (p)
+
+
+def processCipher(data, p):
+    i = 0
+    x = []
+    for j in range(i + p, len(data)):
+        for k in range(i, i + p):
+            for l in range(k + 1, i + p):
+                dejavu = data[j]
+                # validate number and track
+                if data[j] == data[k] + data[l] and data[k] != data[l]:
+                    x.append(data[j])
+
+        # check for first non valid number
+        if data[j] not in x:
+            return data[j]
+        i += 1
+    return False
+
+
+with open('input.txt') as f:
+    data = []
+    for line in f:
+        data.append(int(line.strip()))
+    # Execute processCipher is a preamble of 25
+    print(processCipher(data, 25))
