947579ca8640d78e511b3e227e2c8b9b8ae1714c
Dean C <dcmexy@gmail.com>
2020-12-16 23:54:45 -0500

    Part 1 solution - Day 11

---
+++ day11/solution.py
@@ -0,0 +1,47 @@
+from copy import deepcopy
+
+puzzle = []
+
+# Part 1 of solution - Day 11
+
+
+def process():
+    # Count number of columns
+    colLength = len(puzzle[0])
+    # Count number of rows
+    rowLength = len(puzzle)
+    # Set bounds
+    bounds = [-1, 0, 1]
+
+    while True:
+        puzzle2 = deepcopy(puzzle)
+        totalOccupied = 0
+        # r = row; c = column; rb = rowBound; cb = ColumnBound
+        for r in range(rowLength):
+            for c in range(colLength):
+                adjOccupied = 0
+                for rb in bounds:
+                    for cb in bounds:
+                        if (r + rb >= 0 and r + rb < rowLength) and (c + cb >= 0 and c + cb < colLength):
+                            if not (rb == 0 and cb == 0):
+                                if puzzle2[r + rb][c + cb] == '#':
+                                    adjOccupied += 1
+
+                if adjOccupied == 0 and puzzle[r][c] == 'L':
+                    puzzle[r][c] = '#'
+
+                if puzzle[r][c] == '#':
+                    totalOccupied += 1
+                    if adjOccupied >= 4:
+                        puzzle[r][c] = 'L'
+
+        if puzzle == puzzle2:
+            print("Total Occupied", totalOccupied)
+            break
+
+    # Read data from file
+with open('input.txt') as f:
+    for line in f:
+        puzzle.append(list(line.strip('\n').split(' ')[0]))
+
+    process()
