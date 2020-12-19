9cf0b979fd6b6ed87eb0c69e9b5cb7c1c7083148
Dean C <dcmexy@gmail.com>
2020-12-19 13:46:56 -0500

    Day 11 working solution for both problems

--- day11/solution.py
+++ day11/solution.py
@@ -16,7 +16,6 @@ def process():
     while True:
         puzzle2 = deepcopy(puzzle)
         totalOccupied = 0
-        # r = row; c = column; rb = rowBound; cb = ColumnBound
         for r in range(rowLength):
             for c in range(colLength):
                 adjOccupied = 0
@@ -39,9 +38,54 @@ def process():
             print("Total Occupied", totalOccupied)
             break
 
+
+def process2():
+    # Count number of columns
+    colLength = len(puzzle[0])
+    # Count number of rows
+    rowLength = len(puzzle)
+
+    while True:
+        puzzle2 = deepcopy(puzzle)
+        totalOccupied = 0
+        isSame = True
+        for r in range(rowLength):
+            for c in range(colLength):
+                if puzzle[r][c] == '.':
+                    continue
+                adjOccupied = 0
+                bounds = [-1, 0, 1]
+
+                for rb in bounds:
+                    for cb in bounds:
+                        rbt = r + rb
+                        cbt = c + cb
+                        if rb == 0 and cb == 0:
+                            continue
+                        while (rbt >= 0 and rbt < rowLength) and (cbt >= 0 and cbt < colLength and puzzle2[rbt][cbt] == '.'):
+                            rbt += rb
+                            cbt += cb
+
+                        if (rbt >= 0 and rbt < rowLength) and (cbt >= 0 and cbt < colLength) and puzzle2[rbt][cbt] == '#':
+                            adjOccupied += 1
+                if adjOccupied == 0 and puzzle[r][c] == 'L':
+                    puzzle[r][c] = '#'
+                    isSame = False
+                elif puzzle[r][c] == '#':
+                    totalOccupied += 1
+                    if adjOccupied >= 5:
+                        puzzle[r][c] = 'L'
+                        isSame = False
+
+        if isSame:
+            print("Total Occupied", totalOccupied)
+            break
+
+
     # Read data from file
 with open('input.txt') as f:
     for line in f:
         puzzle.append(list(line.strip('\n').split(' ')[0]))
 
-    process()
+    # process()
+    process2()
